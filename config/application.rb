8efd76b292b05b992a2aa8fe870f6175c5fd8613
NicholasJames <nicholas.james@wegmans.onmicrosoft.com>
2017-11-30 20:07:09 -0500

    Removed api application

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 module ChecklistApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.api_only = true
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
